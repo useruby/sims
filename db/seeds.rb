c30013b73438b420a79bd7f139b5f219cdfeb75e
Alexey Artamonov <i@useruby.com>
2015-10-29 20:44:23 +0100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+if Rails.env.development?
+  admin = FactoryGirl.create(:admin, email: 'admin@test.com', password: 'ooC5aeb1waequ3to')
+  user = FactoryGirl.create(:user, email: 'user@test.com', password: 'kahgahLuGip2ahy8')
+
+  FactoryGirl.create(:product_01, created_by_id: user.id)
+  FactoryGirl.create(:product_02, created_by_id: admin.id)
+  FactoryGirl.create(:product_03, created_by_id: admin.id)
+end
