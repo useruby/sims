cb32af859a6383f1db32fe79fa7b16b6ec9b3807
Alexey Artamonov <i@useruby.com>
2015-10-30 17:40:18 +0100

    added warehouses and product locations in the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,16 @@ if Rails.env.development?
   admin = FactoryGirl.create(:admin, email: 'admin@test.com', password: 'ooC5aeb1waequ3to')
   user = FactoryGirl.create(:user, email: 'user@test.com', password: 'kahgahLuGip2ahy8')
 
-  FactoryGirl.create(:product_01, created_by_id: user.id)
+  main_warehouse = FactoryGirl.create(:main_warehouse)
+  second_facility = FactoryGirl.create(:second_facility)
+
+  FactoryGirl.create(
+    :product_01, created_by_id: user.id, 
+    locations: {main_warehouse => 10, second_facility => 5}
+  )
   FactoryGirl.create(:product_02, created_by_id: admin.id)
-  FactoryGirl.create(:product_03, created_by_id: admin.id)
+  FactoryGirl.create(
+    :product_03, created_by_id: admin.id,
+    locations: {main_warehouse => 5}
+  )
 end
